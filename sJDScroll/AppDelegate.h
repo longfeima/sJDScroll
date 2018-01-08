ebe3a5841981004360da73c048824e42cbc4efac
Seven <caydenk@dragon.local>
2018-01-08 16:52:09 +0800

    Initial Commit

---
+++ sJDScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sJDScroll
+//
+//  Created by CaydenK on 2018/1/8.
+//  Copyright © 2018年 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sJDScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sJDScroll
+//
+//  Created by CaydenK on 2018/1/8.
+//  Copyright © 2018年 Seven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
